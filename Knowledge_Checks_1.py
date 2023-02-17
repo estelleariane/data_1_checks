bef19b2dc4727738aa9158b2d4c46daa095fc2ea
estelleariane <estellesandjong@gmail.com>
2023-02-17 15:57:25 -0500

    change

---
+++ Knowledge_Checks_1.py
@@ -0,0 +1,27 @@
+# A statement that prints "hello world!"
+Greetings = 'Hello World'
+print(Greetings)
+
+
+# A list populated with several values. They can be strings / integers / floats etc. Then, print() one of those values either to the prompt or in a cell (if using Jupyter Notebook)
+
+Car_Manufacturer = ['BMW Group' , 'Daimler AG', 'Ford Motor Co', 'General Motors', 
+'Honda Motor Co', 'Hyundai Motor Group']
+print(Car_Manufacturer[3])
+
+# A dictionary populated with two keys and two values. Print one of the values just like you did above.
+
+State_Capital = { 'Alabama' : 'Montgomery' , 'Alaska' :	'Juneau', 
+'Arizona' : 'Phoenix', 'Arkansas' : 'Little Rock'}
+
+State_population = {'Kentucky' : '746,045', 'Indiana' : '2,492,514', "Georgia" : '6,930,423',
+'Carlifornia' : '2,680,831'}
+
+print(State_Capital)
+
+
+# A tuple with 4 values. Print one of them.
+
+Data_collection = ('Bella', True, (10, 20, 30, 40), ['Good', 'Excellent', 'Bad', 'Poor'])
+print(Data_collection[-3])
+
